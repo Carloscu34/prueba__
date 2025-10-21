68c209f98d09e5950795fbafc9fe73bdd01080ca
Carlos34 <ccum2@miumg.edu.gt>
2025-10-20 23:25:57 -0600

    primer commit

--- Program.cs
+++ Program.cs
@@ -1,25 +1,16 @@
 using Microsoft.EntityFrameworkCore;
-
-builder.Services.AddLogging(loggingBuilder =>
-{
-    loggingBuilder.AddConsole()
-        .AddDebug()
-        .SetMinimumLevel(LogLevel.Information);
-});using Microsoft.EntityFrameworkCore;
 using ApiEmpresa.Models;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-string? cadena ="builder.Configuration.GetConnectionString("DefaultConnection");
-if(cadena!=null){
-    builder.Services.AddDbContext<ApiEmpresaContext>(options =>
-     opt.UseMySQL(cadena));
-}
-builder.Services.AddControllers();
+string? cadena = builder.Configuration.GetConnectionString("DefaultConnection") ?? "otracadena" ;
 
-   
-    //opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); ESTO ES PARA SQL SERVER
+builder.Services.AddControllers();
+builder.Services.AddDbContext<Conexiones>(opt =>
+    opt.UseMySQL(cadena));
+    // esto es para sqlserver
+    //opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); 
+    
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
